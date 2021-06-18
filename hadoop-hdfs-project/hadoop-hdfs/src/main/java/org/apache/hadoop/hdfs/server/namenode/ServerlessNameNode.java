92a99c783f0bbf66418b075a969d16afc444e02e
Scusemua <benrcarver@gmail.com>
2021-06-18 13:04:29 -0400

    Attempting to fix bug, NullPointerException when retrieving DataNodeDataAccess from HdfsStorageFactory in `startServerlessNameNode()`

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/ServerlessNameNode.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/ServerlessNameNode.java
@@ -441,7 +441,7 @@ public class ServerlessNameNode implements NameNodeStatusMXBean {
       // Retrieve the DataNodes from intermediate storage.
       LOG.info("Retrieving list of DataNodes from intermediate storage now...");
       DataNodeDataAccess<DataNodeMeta> dataAccess = (DataNodeDataAccess<DataNodeMeta>)
-              HdfsStorageFactory.getDataAccess(DataNodeMeta.class);
+              HdfsStorageFactory.getDataAccess(DataNodeDataAccess.class);
       List<DataNodeMeta> dataNodes = dataAccess.getAllDataNodes();
       LOG.info("Retrieved list of DataNodes from intermediate storage with " + dataNodes.size() + " entries!");
 
