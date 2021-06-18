f973cb7dae2243c5725b952ebc51315c4223113d
Scusemua <benrcarver@gmail.com>
2021-06-18 13:16:42 -0400

    Updated schema.sql in hops-metadata-dal-impl-ndb to include datanode table.

--- hops-metadata-dal-impl-ndb/schema/schema.sql
+++ hops-metadata-dal-impl-ndb/schema/schema.sql
@@ -22,6 +22,16 @@ CALL flyway$$
 
 delimiter $$
 
+CREATE TABLE `datanodes` (
+    `datanode_uuid` varchar(36) NOT NULL,
+    `hostname` varchar(253) NOT NULL,
+    `ipaddr` varchar(15) NOT NULL,
+    `xfer_port` int(11),
+    `info_port` int(11),
+    `ipc_port` int(11),
+    PRIMARY KEY (`datanode_uuid`)
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs
+
 CREATE TABLE `hdfs_block_infos` (
   `inode_id` int(11) NOT NULL,
   `block_id` bigint(20) NOT NULL,
