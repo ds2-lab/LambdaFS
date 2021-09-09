f62b523dcdf983e1f50e1f2bc886b2d479a2dac2
Scusemua <benrcarver@gmail.com>
2021-09-08 21:50:24 -0400

    Modified worker thread to discard duplicate tasks.

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/serverless/NameNodeWorkerThread.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/serverless/NameNodeWorkerThread.java
@@ -1,13 +1,18 @@
 package org.apache.hadoop.hdfs.serverless;
 
 import org.apache.hadoop.hdfs.server.namenode.ServerlessNameNode;
+import org.eclipse.jetty.util.ConcurrentHashSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * This thread actually executes file system operations. Tasks (i.e., file system operations wrapped in a Future
@@ -27,10 +32,23 @@ public class NameNodeWorkerThread implements Runnable {
      */
     private final BlockingQueue<FileSystemTask<Serializable>> workQueue;
 
+    /**
+     * All tasks that are currently being executed. This is probably just going to be one at a time.
+     */
+    private final ConcurrentHashMap<String, FileSystemTask<Serializable>> currentlyExecutingTasks;
+
+    /**
+     * All tasks that have been executed by this worker thread.
+     */
+    private final ConcurrentHashMap<String, FileSystemTask<Serializable>> completedTasks;
+
     public NameNodeWorkerThread(BlockingQueue<FileSystemTask<Serializable>> workQueue,
                                 ServerlessNameNode serverlessNameNodeInstance) {
         this.serverlessNameNodeInstance = serverlessNameNodeInstance;
         this.workQueue = workQueue;
+
+        this.currentlyExecutingTasks = new ConcurrentHashMap<>();
+        this.completedTasks = new ConcurrentHashMap<>();
     }
 
     @Override
@@ -45,16 +63,29 @@ public class NameNodeWorkerThread implements Runnable {
                 LOG.debug("Worker thread: dequeued task " + task.getTaskId() + "(operation = "
                                 + task.getOperationName() + ").");
 
+                // Check if this is a duplicate task. If it is, then just discard it and move on.
+                if (isTaskDuplicate(task)) {
+                    LOG.warn("Encountered duplicate task " + task.getTaskId() + " (operation = "
+                            + task.getOperationName() + ").");
+                    continue;
+                }
+
+                currentlyExecutingTasks.put(task.getTaskId(), task);
+
                 Serializable result = null;
                 synchronized (serverlessNameNodeInstance) {
                     result = serverlessNameNodeInstance.performOperation(task.getOperationName(), task.getOperationArguments());
                 }
+
+                currentlyExecutingTasks.remove(task.getTaskId());
 
                 // Do something with the task.
                 if (result != null)
                     task.postResult(result);
                 else
                     task.postResult(NullResult.getInstance());
+
+                completedTasks.put(task.getTaskId(), task);
             }
         }
         catch (InterruptedException ex) {
@@ -64,6 +95,47 @@ public class NameNodeWorkerThread implements Runnable {
             LOG.error("Encountered " + ex.getClass().getSimpleName() + " while executing task " +
                     task.getTaskId() + " (operation = " + task.getOperationName() + ").", ex);
         }
+    }
+
+    /**
+     * Returns the number of tasks currently being executed by the worker thread.
+     *
+     * Generally speaking, this should just be one.
+     */
+    public synchronized int numTasksCurrentlyExecuting() {
+        return currentlyExecutingTasks.size();
+    }
+
+    /**
+     * Returns the number of tasks that have been executed by this worker thread.
+     */
+    public synchronized int numTasksExecuted() {
+        return completedTasks.size();
+    }
+
+    /**
+     * Check if this task is a duplicate based on its task key. The task key comes from the request ID, which
+     * is generated client-side. This means that, for a given HTTP request, its corresponding TCP request will have
+     * the same request ID (and vice versa).
+     * @param candidate the task for which we are checking if it is a duplicate
+     * @return true if the task is a duplicate, otherwise false.
+     */
+    public synchronized boolean isTaskDuplicate(FileSystemTask<Serializable> candidate) {
+        return currentlyExecutingTasks.containsKey(candidate.getTaskId())
+                || completedTasks.containsKey(candidate.getTaskId());
+    }
+
+    /**
+     * Get the duplicate task of the given task.
+     *
+     * For example, if this function is passed a task corresponding to an HTTP request with ID "FOO", then this will
+     * return the TCP request with ID "FOO".
+     */
+    public synchronized FileSystemTask<Serializable> getDuplicateTask(FileSystemTask<Serializable> candidate) {
+        if (currentlyExecutingTasks.containsKey(candidate.getTaskId()))
+            return currentlyExecutingTasks.get(candidate.getTaskId());
+
+        return completedTasks.get(candidate.getTaskId());
     }
 
     /**
