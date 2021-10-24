e1ed5d60f9d72ccea3e789f5c64aa424a628b29b
Scusemua <benrcarver@gmail.com>
2021-10-24 19:23:25 -0400

    Only print something in func preventStorageCall of EntityContext if the value of al differs from that of storageCallPrevented

--- hops-metadata-dal/src/main/java/io/hops/transaction/context/EntityContext.java
+++ hops-metadata-dal/src/main/java/io/hops/transaction/context/EntityContext.java
@@ -218,18 +218,13 @@ public abstract class EntityContext<T> {
   }
 
   public void preventStorageCall(boolean val) {
-    storageCallPrevented = val;
+    if (val != storageCallPrevented) {
+      LOG.warn("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+      LOG.warn("Storage calls " + (val ? "WILL" : "will NOT") + " be PREVENTED from this point forward.");
+      LOG.warn("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+    }
 
-    if (val) {
-      LOG.warn("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-      LOG.warn("Storage calls will be PREVENTED from this point forward.");
-      LOG.warn("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-    }
-    else {
-      LOG.warn("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-      LOG.warn("Storage calls will NO LONGER be prevented.");
-      LOG.warn("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-    }
+    storageCallPrevented = val;
   }
 
   public static void setLockMode(LockMode lock) {
