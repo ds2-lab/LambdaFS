7572fe833b84b34760003abd8b265913073867b5
Scusemua <benrcarver@gmail.com>
2021-11-06 21:41:52 -0400

    Trying again to get format correct

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/serverless/invoking/ServerlessNameNodeClient.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/serverless/invoking/ServerlessNameNodeClient.java
@@ -1526,7 +1526,7 @@ public class ServerlessNameNodeClient implements ClientProtocol {
 
         @Override
         public String toString() {
-            String format = "%-30s %-30s %-30s %-10s %-12s %-6s %-6s";
+            String format = "%-25s %-25s %-25s %-12s %-12s %-6s %-6s";
 
             return String.format(format, operationName, Instant.ofEpochMilli(startTime).toString(),
                     Instant.ofEpochMilli(endTime).toString(), duration, deployment,
