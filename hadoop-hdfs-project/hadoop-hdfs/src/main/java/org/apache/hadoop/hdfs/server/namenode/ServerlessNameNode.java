a04cef3a51bf146b873bfb253963c3ad5384614f
Scusemua <benrcarver@gmail.com>
2021-11-15 20:00:14 -0500

    Updated version number

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/ServerlessNameNode.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/ServerlessNameNode.java
@@ -234,7 +234,7 @@ public class ServerlessNameNode implements NameNodeStatusMXBean {
    * Syntax:
    *  Major.Minor.Build.Revision
    */
-  public static final String versionNumber = "0.3.3.0";
+  public static final String versionNumber = "0.3.4.0";
 
   /**
    * The number of uniquely-deployed serverless name nodes associated with this particular Serverless HopsFS cluster.
