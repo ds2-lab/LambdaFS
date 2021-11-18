ff74b2f79f9a815f53ae87bd53d7d667c80efb77
Scusemua <benrcarver@gmail.com>
2021-11-18 18:35:09 -0500

    Changed prints about max tries for HTTP invocations

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/serverless/invoking/OpenWhiskInvoker.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/serverless/invoking/OpenWhiskInvoker.java
@@ -248,7 +248,7 @@ public class OpenWhiskInvoker extends ServerlessInvokerBase<JsonObject> {
 
         do {
             LOG.info("Invoking NameNode " + targetDeployment + " (op=" + operationName + "), attempt "
-                    + (exponentialBackoff.getNumberOfRetries() - 1) + "/" + (maxHttpRetries + 1) + ".");
+                    + (exponentialBackoff.getNumberOfRetries() - 1) + "/" + maxHttpRetries + ".");
 
             CloseableHttpResponse httpResponse;
             try {
@@ -283,7 +283,7 @@ public class OpenWhiskInvoker extends ServerlessInvokerBase<JsonObject> {
             // yet, but if you try again a few seconds later, then the request will get through.
             if (responseCode >= 400 && responseCode <= 599) {
                 LOG.error("Received HTTP response code " + responseCode + " on attempt " +
-                        (exponentialBackoff.getNumberOfRetries()) + "/" + (maxHttpRetries + 1) + ".");
+                        (exponentialBackoff.getNumberOfRetries()) + "/" + maxHttpRetries + ".");
                 LOG.warn("Sleeping for " + backoffInterval + " milliseconds before trying again...");
                 doSleep(backoffInterval);
                 backoffInterval = exponentialBackoff.getBackOffInMillis();
@@ -300,7 +300,7 @@ public class OpenWhiskInvoker extends ServerlessInvokerBase<JsonObject> {
         } while (backoffInterval != -1);
 
         throw new IOException("The file system operation could not be completed. " +
-                "Failed to invoke a Serverless NameNode after " + (maxHttpRetries + 1) + " attempts.");
+                "Failed to invoke a Serverless NameNode after " + maxHttpRetries + " attempts.");
     }
 
     /**
