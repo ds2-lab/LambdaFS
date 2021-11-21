ebad9e03ef86e01f48412997ba1682b72de877cf
Scusemua <benrcarver@gmail.com>
2021-11-21 10:24:04 -0500

    Moved printing of TransactionsStats.TransactionStat object behind null check

--- hops-metadata-dal/src/main/java/io/hops/transaction/handler/TransactionalRequestHandler.java
+++ hops-metadata-dal/src/main/java/io/hops/transaction/handler/TransactionalRequestHandler.java
@@ -171,9 +171,8 @@ public abstract class TransactionalRequestHandler extends RequestHandler {
         commitTime = (System.currentTimeMillis() - oldTime);
         if(stat != null){
           stat.setTimes(acquireLockTime, inMemoryProcessingTime, commitTime);
+          requestHandlerLOG.debug("TransactionStat: " + stat.toString());
         }
-
-        requestHandlerLOG.debug("TransactionStat: " + stat.toString());
 
         if(requestHandlerLOG.isTraceEnabled()) {
           requestHandlerLOG.debug("TX committed. Time " + commitTime + " ms");
