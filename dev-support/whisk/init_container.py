809bc644d76d8d263897e72a8e13acf94a6e9519
Scusemua <benrcarver@gmail.com>
2021-12-08 14:40:23 -0500

    Modified host parameter to not use -h as that conflicts with the default help param

--- dev-support/whisk/init_container.py
+++ dev-support/whisk/init_container.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-p", "--port", type = int, default = 50010)
-    parser.add_argument("-h", "--host", type = str, default = "127.0.0.1")
+    parser.add_argument("--host", type = str, default = "127.0.0.1")
     parser.add_argument("-m", "--main", type = str, default = "org.apache.hadoop.hdfs.server.namenode.ServerlessNameNode")
     parser.add_argument("-j", "--jar-path", dest = "jar_path", type = str, default = "/home/ubuntu/repos/hops/hadoop-hdfs-project/hadoop-hdfs/target/hadoop-hdfs-3.2.0.3-SNAPSHOT.jar")
 
