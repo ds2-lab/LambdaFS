194922a29cf3939199675516592965d527707451
Scusemua <benrcarver@gmail.com>
2021-12-08 14:39:50 -0500

    Added 'http://' and made the hostname a parameter

--- dev-support/whisk/init_container.py
+++ dev-support/whisk/init_container.py
@@ -6,18 +6,22 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-p", "--port", type = int, default = 50010)
+    parser.add_argument("-h", "--host", type = str, default = "127.0.0.1")
     parser.add_argument("-m", "--main", type = str, default = "org.apache.hadoop.hdfs.server.namenode.ServerlessNameNode")
     parser.add_argument("-j", "--jar-path", dest = "jar_path", type = str, default = "/home/ubuntu/repos/hops/hadoop-hdfs-project/hadoop-hdfs/target/hadoop-hdfs-3.2.0.3-SNAPSHOT.jar")
 
     args = parser.parse_args()
 
     port = args.port
+    host = args.host
     main = args.main
     jar_path = args.jar_path
 
     with open(jar_path, 'rb') as jar_file:
         base64_encoded_jar_file = base64.b64encode(jar_file.read()).decode('utf-8')
-        r = requests.post("127.0.0.1:%d/init" % port, json = {
+        endpoint = "http://%s:%d/init" % (host, port)
+        print("Issuing POST request to '%s' now." % endpoint)
+        r = requests.post(endpoint, json = {
             "value": {
                 "main": main,
                 "code": base64_encoded_jar_file
