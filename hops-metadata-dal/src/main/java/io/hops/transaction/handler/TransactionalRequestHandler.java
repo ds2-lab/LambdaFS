4ff4c6a29d1b782750e069d50d45d01dbd446e12
Scusemua <benrcarver@gmail.com>
2022-02-02 18:27:44 -0500

    Temporarily disabled the consistency protocol

--- hops-metadata-dal/src/main/java/io/hops/transaction/handler/TransactionalRequestHandler.java
+++ hops-metadata-dal/src/main/java/io/hops/transaction/handler/TransactionalRequestHandler.java
@@ -169,7 +169,7 @@ public abstract class TransactionalRequestHandler extends RequestHandler {
             .collectStats(opType,
             ignoredException);
 
-        boolean canProceed = consistencyProtocol(txStartTime, transactionAttempt);
+        boolean canProceed = true; // consistencyProtocol(txStartTime, transactionAttempt);
 
         consistencyProtocolTime = (System.currentTimeMillis() - oldTime);
         transactionAttempt.setConsistencyProtocolEnd(getUtcTime());
