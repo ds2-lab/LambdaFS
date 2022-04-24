f566fbdaa737a255f4e1e008b065875491db66e1
Scusemua <benrcarver@gmail.com>
2022-04-24 16:42:05 -0400

    The Docker container often fails on its first try when instantiating the HopsSessionFactory but is successful on a subsequent invocation. I am going to try wrapping the instantiation in a loop to see if that helps.

--- hops-metadata-dal-impl-ndb/src/main/java/io/hops/metadata/ndb/DBSessionProvider.java
+++ hops-metadata-dal-impl-ndb/src/main/java/io/hops/metadata/ndb/DBSessionProvider.java
@@ -76,18 +76,24 @@ public class DBSessionProvider implements Runnable {
     LOG.info("Database name: " + conf.get(Constants.PROPERTY_CLUSTER_DATABASE));
     LOG.info("Max Transactions: " + conf.get(Constants.PROPERTY_CLUSTER_MAX_TRANSACTIONS));
 
-    try {
+    int retries = 1;
 
-      if (sessionFactory != null) {
-        LOG.debug("HopsSessionFactory instance is already instantiated. Reusing existing object.");
-      } else {
-        LOG.debug("Instantiation HopsSessionFactory object now...");
-        sessionFactory = new HopsSessionFactory(ClusterJHelper.getSessionFactory(conf));
-        LOG.debug("Instantiation of HopsSessionFactory was successful!");
+    while (retries >= 0) {
+      try {
+        if (sessionFactory != null) {
+          LOG.debug("HopsSessionFactory instance is already instantiated. Reusing existing object.");
+        } else {
+          LOG.debug("Instantiation HopsSessionFactory object now...");
+          sessionFactory = new HopsSessionFactory(ClusterJHelper.getSessionFactory(conf));
+          LOG.debug("Instantiation of HopsSessionFactory was successful!");
+        }
+      } catch (ClusterJException ex) {
+        LOG.error("Exception encountered while instantiation HopsSessionFactory instance: ", ex);
+
+        // Only throw the exception if we aren't going to try again.
+        if (--retries < 0)
+          throw HopsExceptionHelper.wrap(ex);
       }
-    } catch (ClusterJException ex) {
-      LOG.error("Exception encountered while instantiation HopsSessionFactory instance: ", ex);
-      throw HopsExceptionHelper.wrap(ex);
     }
 
     LOG.debug("Initializing " + initialPoolSize + " session(s) now...");
